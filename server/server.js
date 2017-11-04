f7543da71fc8d11aef3bf7d71c6fcd4848d9758f
Ion Petrache <ionpetrache@socialsweethearts.de>
2017-11-04 23:49:01 +0100

    Add models, create first endpoint

--- server/server.js
+++ server/server.js
@@ -1,54 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
+var app = express();
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
 });
 
-// var secondTodo = new Todo({
-//     text: 'Learning NodeJS',
-//     completed: false,
-//     completedAt: Date.now()
-// });
-
-// secondTodo.save().then((doc) => {
-//     console.log('Save 2nd todo:', doc);
-// }, (err) => {
-//     console.log('Unable to save 2nd todo', err);
-// });
-
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        trim: true,
-        minlength: 1,
-        required: true
-    }
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-var newUser = new User({
-    email: 'gigi.boschetaru@gmail.com '
-});
-
-newUser.save().then((doc) => {
-    console.log('Saving the new user:', doc);
-}, (err) => {
-    console.log('Error saving the new user', err);
-})
